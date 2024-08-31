9c76f3c13db45fc920044da9e21d7066070e2702
Eduard - Eugen Ocunschi <eduardocunschi@gmail.com>
2024-08-31 14:42:34 +0200

    Third Commit - Adding Extra Functionality to Open/Close Modal

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-const openModal = document.querySelector(".add-btn");
-const modal = document.querySelector(".modal");
+const openModalBtn = document.querySelector(".add-btn");
 const contentContainer = document.querySelector(".content-container");
 const modalContainer = document.querySelector(".modal-container");
+const modalOverlay = document.querySelector(".modal-overlay");
 
 const cancelModal = document.querySelector(".modal-btn-cancel");
 
 window.onload = () => {
-  // Open Modal
-  openModal.addEventListener("click", () => {
+  // FUNCTIONALITY
+  const openModalFunction = function () {
     setTimeout(() => {
-      modal.classList.remove("hidden");
-      modal.classList.add("animation-bgIn");
+      modalContainer.classList.remove("hidden");
       modalContainer.classList.add("animationModal-moveIn");
-      contentContainer.classList.add("opacity");
-      contentContainer.classList.remove("animation-moveIn");
+      modalOverlay.classList.remove("hidden");
+      modalOverlay.classList.add("animation-bgIn");
+      // contentContainer.classList.add("opacity");
+      // contentContainer.classList.remove("animation-moveIn");
     }, 100);
-  });
+  };
 
-  // Close Modal
-  cancelModal.addEventListener("click", () => {
+  const closeModalFunction = function () {
     setTimeout(() => {
       modalContainer.classList.remove("animationModal-moveIn");
-      modal.classList.add("animation-bgOut");
       modalContainer.classList.add("animation-moveOut");
+      modalOverlay.classList.add("animation-bgOut");
       setTimeout(() => {
-        modal.classList.add("hidden");
+        modalContainer.classList.add("hidden");
+        modalOverlay.classList.add("hidden");
       }, 601);
       //test
       // addEventListener("animationend", () => {
@@ -33,10 +34,50 @@ window.onload = () => {
 
       setTimeout(() => {
         modalContainer.classList.remove("animation-moveOut");
-      }, 601);
-      setTimeout(() => {
-        modal.classList.remove("animation-bgOut");
+        modalOverlay.classList.remove("animation-bgOut");
       }, 601);
     }, 180);
+  };
+  // Open Modal
+  openModalBtn.addEventListener("click", openModalFunction);
+
+  // openModalBtn.addEventListener("click", () => {
+  //   setTimeout(() => {
+  //     modalContainer.classList.remove("hidden");
+  //     modalContainer.classList.add("animationModal-moveIn");
+  //     modalOverlay.classList.remove("hidden");
+  //     modalOverlay.classList.add("animation-bgIn");
+  //     contentContainer.classList.add("opacity");
+  //     contentContainer.classList.remove("animation-moveIn");
+  //   }, 100);
+  // });
+
+  // Close Modal
+  cancelModal.addEventListener("click", closeModalFunction);
+  modalOverlay.addEventListener("click", closeModalFunction);
+  document.addEventListener("keydown", function (btn) {
+    if (btn.key === "Escape" && !modalContainer.classList.contains("hidden")) {
+      closeModalFunction();
+    }
   });
+  // cancelModal.addEventListener("click", () => {
+  //   setTimeout(() => {
+  //     modalContainer.classList.remove("animationModal-moveIn");
+  //     modalContainer.classList.add("animation-moveOut");
+  //     modalOverlay.classList.add("animation-bgOut");
+  //     setTimeout(() => {
+  //       modalContainer.classList.add("hidden");
+  //       modalOverlay.classList.add("hidden");
+  //     }, 601);
+  //     //test
+  //     // addEventListener("animationend", () => {
+  //     //   modal.classList.add("hidden");
+  //     // });
+
+  //     setTimeout(() => {
+  //       modalContainer.classList.remove("animation-moveOut");
+  //       modalOverlay.classList.remove("animation-bgOut");
+  //     }, 601);
+  //   }, 180);
+  // });
 };
